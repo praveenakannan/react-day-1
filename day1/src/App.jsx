105631888cf7ee7b27a96e9c2b30bcbead8f3656
praveenakannan <praveenakannan.p@gmail.com>
2024-07-07 15:19:51 +0530

    change

---
+++ day1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './component/Card';
+import Price from './component/Price';
+const App = () => {
+  return (
+    <div>
+      <Card/>
+      <Price/>
+    </div>
+  );
+};
+
+export default App;
