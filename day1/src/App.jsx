b6b74618ddeb07300a68636221ce19aeb1a98820
praveenakannan <praveenakannan.p@gmail.com>
2024-07-07 18:35:53 +0530

    Update App.jsx

--- day1/src/App.jsx
+++ day1/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Card from './component/Card';
 import Price from './component/Price';
 const App = () => {
   return (
     <div>
-      <Card/>
+      
       <Price/>
     </div>
   );
